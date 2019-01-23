a906178371950c0674bddc96fb11e5454aae6ff9
Marta Paz <pazmarta@gmail.com>
2019-01-23 12:46:51 +0000

    add JSON parse

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-// create new variable
+// create new variable (create new instance of the XMLHttpRequest() object)
 var xhr = new XMLHttpRequest();
+
+// open a connection to communicate with the web server
+xhr.open("GET", "https://swapi.co/api/");
+
+xhr.send();
 
 // listener to see xhr state:
 // create a new function | check whenever the state changes | if all is ok retrieve our data div
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        // document.getElementById("data").innerHTML = this.responseText;
+        // console.log(JSON.parse(this.responseText));
+        data = this.responseText;
     }
 };
 
+console.log(data);
 
-// open a connection
-xhr.open("GET", "https://swapi.co/api/");
 
-xhr.send();
